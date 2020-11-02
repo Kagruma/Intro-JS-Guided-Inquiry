a26b9152a19008d43c2d19f40b33e77dfb114b35
Kagruma <nguyene375@gmail.com>
2020-11-01 23:27:26 -0500

    "finished easy challenge"

--- Easy/script.js
+++ Easy/script.js
@@ -1,6 +1,9 @@
-var num1 = 9
-var num2 = 2
+var str1 = "Calvin Coleman"
+var str2 = "Jana"
+var n1 = str1.length;
+var n2 = str2.length;
 
-var num3 = num1 - num2
+var x = n1 - n2
+console.log("The name Thomas is longer than John by " + x + " characters " )
 
-console.log("The difference between " + num1 + " and " + num2 + " is " + num3)
+
